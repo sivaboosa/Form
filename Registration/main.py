5c2f57a4404840e31330b4a718c6925be8eab56e
sivasiddu <sivasiddu61@gmail.com>
2021-08-18 15:13:30 +0100

    changes

--- Registration/main.py
+++ Registration/main.py
@@ -6,7 +6,8 @@ app = Flask(__name__, template_folder = "template")
 
 mysql = MySQL(app)
 
-@app.route('/form', methods=['GET','POST'])
+@app.route('/')
+@app.route('/form', methods = ['GET', 'POST'])
 
 def hello():
     if request.method == 'POST':
@@ -17,21 +18,21 @@ def hello():
         Email = userdetails['Email']   
         Phonenumber = userdetails['Number']
         Password = userdetails['Pass']
-        Gender = userdetails['gender']
+        Gender = userdetails['Gender']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s, %s, %d, %s, %s)",(Firstname, Lastname, Email, Phonenumber, Password, Gender))
+        cur.execute("INSERT INTO registration(Firstname, Lastname, Email, Phonenumber, Password, Gender) VALUES(%s, %s, %s, %d, %s, %s)",(Firstname, Lastname, Email, Phonenumber, Password, Gender))
         mysql.connection.commit()
         cur.close()
-        return redirect('/registration')
+        return redirect('/success')
     return render_template('form.html')
  
-@app.route('/success')
+@app.route('/registration')
 def users():
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM users")
     if resultValue > 0:
-        userDetails = cur.fetchall()
-        return render_template('success.html',userDetails = userDetails)
+        userdetails = cur.fetchall()
+        return render_template('success.html', userdetails = userdetails)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost')
