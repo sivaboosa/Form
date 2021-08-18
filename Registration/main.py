3bc0a9fbd5f5344ebb812fb924eb58dd97ba0b20
sivasiddu <sivasiddu61@gmail.com>
2021-08-18 13:21:51 +0100

    withflask

---
+++ Registration/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
+
+def hello():
+    if request.method == 'POST':
+        #Fetch form data
+        userDetails = request.form
+        Firstname = userDetails.getvalue("Firstname")
+        Lastname = userDetails.getvalue("Lastname")
+        Email = userDetails.getvalue("Email")
+        Phonenumber = userDetails.getvalue("Number")
+        Password = userDetails.getvalue("Pass")
+        Gender = userDetails.getvalue("Gender")
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s, %s, %d, %s, %s)",(Firstname, Lastname, Email, Phonenumber, Password, Gender))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/registration')
+    return render_template('formm.html')
+ 
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('success.html',userDetails = userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost')
---
+++ flask.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
+
+def hello():
+    if request.method == 'POST':
+        #Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/users')
+    return render_template('index.html')
+ 
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost')
