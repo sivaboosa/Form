67308df41c8540cc8d885db4ebe65b4b4f594041
sivasiddu <sivasiddu61@gmail.com>
2021-08-25 15:15:57 +0100

    update

--- Registration/main.py
+++ Registration/main.py
@@ -1,3 +1,4 @@
+from html.entities import html5
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
@@ -6,23 +7,23 @@ app = Flask(__name__, template_folder = "template")
 
 mysql = MySQL(app)
 
-@app.route("/form.html", methods = ['GET', 'POST'])
+@app.route("/", methods = ['POST'])
 
 def registration():
     if request.method == 'POST':
         #Fetch form data
         userdetails = request.form
-        Firstname = userdetails['Firstname']
+        Firstname = userdetails["Firstname"]
         Lastname = userdetails['Lastname']
         Email = userdetails['Email']   
         Phonenumber = userdetails['Number']
         Password = userdetails['Pass']
         Gender = userdetails['Gender']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO registration(Firstname, Lastname, Email, Phonenumber, Password, Gender) VALUES(%s, %s, %s, %d, %s, %s)",(Firstname, Lastname, Email, Phonenumber, Password, Gender))
+        cur.execute("INSERT INTO registration(Firstname, Lastname, Email, Phonenumber, Password, Gender) VALUES(%s, %s, %s, %s, %s, %s)",(Firstname, Lastname, Email, Phonenumber, Password, Gender))
         mysql.connection.commit()
         cur.close()
-        return redirect('/success')
+        return redirect('success.html')
     return render_template('form.html')
  
 @app.route('/success')
